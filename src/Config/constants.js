ac903b7f2c5de0022a291833d77f906289ae2373
Ezra <efeldman207@gmail.com>
2023-06-01 11:22:44 +0300

    deployment bug fix

--- src/Config/constants.js
+++ src/Config/constants.js
@@ -1,7 +1,7 @@
 const dev = {
-    API_URL: "http://localhost:8080/"
+    API_URL: "http://localhost:8080"
 };
 const prod = { 
-    API_URL: "https://hackathon-event-hop.herokuapp.com/"
+    API_URL: "https://hackathon-event-hop.herokuapp.com"
 };
 export const config = process.env.NODE_ENV === "development" ? dev : prod;
