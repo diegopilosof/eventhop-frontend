0cdcc1ae5b4164299e28a4a8ab92264608d66184
Ezra <efeldman207@gmail.com>
2023-06-01 10:50:56 +0300

    deploying

--- src/Config/constants.js
+++ src/Config/constants.js
@@ -2,6 +2,6 @@ const dev = {
     API_URL: "http://localhost:8080/"
 };
 const prod = { 
-    API_URL: "bla"
+    API_URL: "https://hackathon-event-hop.herokuapp.com/"
 };
 export const config = process.env.NODE_ENV === "development" ? dev : prod;
